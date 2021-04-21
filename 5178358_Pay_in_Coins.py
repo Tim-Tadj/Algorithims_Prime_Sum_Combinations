b7f24bf363c7d7a6ddd9b61a0ce21230cf27dc32
Tim-Tadj <77534029+Tim-Tadj@users.noreply.github.com>
2021-04-21 16:46:42 +1000

    changed to sieve

--- 5178358_Pay_in_Coins.py
+++ 5178358_Pay_in_Coins.py
@@ -1,6 +1,30 @@
 import os
 import sys
 import time
+import itertools
+def SieveOfEratosthenes(n):
+    primes = [1]
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is Not a prime, else true.
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+          
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+              
+            # Update all multiples of p
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0]= False
+    prime[1]= False
+    # Print all prime numbers
+    for p in range(n + 1):
+        if prime[p]:
+            primes.append(p)
+    return primes
 
 def primeSum(data):
     #initialise varibles 
@@ -13,18 +37,12 @@ def primeSum(data):
     if len(data) > 2:
         upperBound = data[2]
         upperExists = True
-
-    primes = [] #possible primes stored here
-    for num in range(1,sumToReach+1):  #find all primes up to the sumToReach
-        if num > 1:  
-            for i in range(2,num):  #for every number try to see if it can be divided
-                if (num % i) == 0:  #break if we find a non-prime number
-                    break  
-            else:  #execute when for loop doesn't break
-                primes.append(num)
+    
+    primes = SieveOfEratosthenes(sumToReach)
     print(primes)
-                
-    results = []
+    result = [seq for i in range(len(primes)) for seq in itertools.combinations(primes, i) if sum(seq) == sumToReach]
+    print(result)
+    input()
     
 
 
@@ -34,8 +52,9 @@ def read_file(Input):
             stringdata = line.rstrip("\n").split(" ") #create a var for each number
             integer_map = map(int, stringdata) #convert list of strings to map of ints
             data = list(integer_map) #convert map to list
-            print(data)
+            # print(data)
             primeSum(data)
+            
 
 
 read_file("Input.txt")

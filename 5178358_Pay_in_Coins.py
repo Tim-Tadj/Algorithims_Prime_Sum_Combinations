2f1bcb83c0c78398d77abddc38ef1e3f601deee2
Tim-Tadj <77534029+Tim-Tadj@users.noreply.github.com>
2021-04-23 17:29:58 +1000

    working

--- 5178358_Pay_in_Coins.py
+++ 5178358_Pay_in_Coins.py
@@ -2,48 +2,70 @@ import os
 import sys
 import time
 import itertools
-def SieveOfEratosthenes(n):
-    primes = [1]
-    # Create a boolean array "prime[0..n]" and initialize
-    # all entries it as true. A value in prime[i] will
-    # finally be false if i is Not a prime, else true.
-    prime = [True for i in range(n + 1)]
-    p = 2
-    while (p * p <= n):
-          
-        # If prime[p] is not changed, then it is a prime
-        if (prime[p] == True):
-              
-            # Update all multiples of p
-            for i in range(p * 2, n + 1, p):
-                prime[i] = False
-        p += 1
-    prime[0]= False
-    prime[1]= False
-    # Print all prime numbers
-    for p in range(n + 1):
-        if prime[p]:
-            primes.append(p)
-    return primes
+import math
+import copy
 
-def primeSum(data):
-    #initialise varibles 
-    sumToReach = data[0]
-    lowerExists = False
-    upperExists = False
-    if len(data) > 1:
-        lowerBound = data[1]
-        lowerExists = True
-    if len(data) > 2:
-        upperBound = data[2]
-        upperExists = True
-    
-    primes = SieveOfEratosthenes(sumToReach)
-    print(primes)
-    result = [seq for i in range(len(primes)) for seq in itertools.combinations(primes, i) if sum(seq) == sumToReach]
-    print(result)
-    input()
-    
+def isprime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    else:
+        return True
+
+def SieveOfEratosthenes(number):
+
+    primes = []
+    for i in range(2,number+1):
+        primes.append(i)
+
+    i = 2
+    #from 2 to sqrt(number)
+    while(i <= int(math.sqrt(number))):
+        #if i is in list
+        #then we gotta delete its multiples
+        if i in primes:
+            #j will give multiples of i,
+            #starting from 2*i
+            for j in range(i*2, number+1, i):
+                if j in primes:
+                    #deleting the multiple if found in list
+                    primes.remove(j)
+        i = i+1
+
+    return (primes)
+
+def sum_rec(target, current_sum, start, output, result):
+    if current_sum == target:
+        output.append(copy.copy(result))
+
+    for i in range(start, target):
+        temp_sum = current_sum + i   
+        if not isprime(i):
+            continue
+        if temp_sum <= target:
+            result.append(i)
+            sum_rec( target, temp_sum, i, output, result)
+            result.pop()
+        else:
+            return
+
+def sum(target):
+    output = []
+    result = []
+    sum_rec(target, 0, 1, output, result)
+    return output
+
+def primesum(data):
+    primes = SieveOfEratosthenes(data[0])
+    if len(data) == 3:
+        pass
+    elif len(data) == 2:
+        pass
+    elif len(data) ==  1:
+        return sum(data[0])
+    else:
+        print("Invalid input file")
+
 
 
 def read_file(Input):
@@ -52,9 +74,8 @@ def read_file(Input):
             stringdata = line.rstrip("\n").split(" ") #create a var for each number
             integer_map = map(int, stringdata) #convert list of strings to map of ints
             data = list(integer_map) #convert map to list
-            # print(data)
-            primeSum(data)
+            #print(data)
+            print(len(primesum(data)))
             
-
 
 read_file("Input.txt")
